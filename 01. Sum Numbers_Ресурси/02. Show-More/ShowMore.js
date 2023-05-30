40e3e0820c0ba8126d380ed948047170e2137d7f
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2023-05-30 22:32:09 +0300

    Delete 01. Sum Numbers_Ресурси directory

--- 01. Sum Numbers_Ресурси/02. Show-More/ShowMore.js
+++
@@ -1,6 +0,0 @@
-function showText() {
-    let text = document.getElementById("text");
-    text.style.display = 'inline';
-    let moreText = document.getElementById("more");
-    moreText.style.display = 'none';
-}
--- 01. Sum Numbers_Ресурси/03. Collect-List-Items/CollectListItems.js
+++
@@ -1,5 +0,0 @@
-function extractText() {
-    let elements = document.getElementById('items');
-    let result = document.getElementById('result');
-    result.textContent = elements.textContent;
-}
